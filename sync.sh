6fd055c4e32df3de0abd14f95016681e12313361
madkarmaa <madkarma99@gmail.com>
2026-06-16 13:13:09 +0200

    refactor: move newline

--- sync.sh
+++ sync.sh
@@ -10,8 +10,8 @@ cp OneDark-Pro/themes/*.json themes/tmp/
 [ -d zed ] || git clone https://github.com/zed-industries/zed.git --depth 1
 
 cd zed
-cargo build -p theme_importer
 
+cargo build -p theme_importer
 BIN=target/debug/theme_importer
 
 for theme in ../themes/tmp/OneDark-Pro*.json; do
